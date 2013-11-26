87deea655d33be0e07340531d109f974d63cc976
Lenka Bacinska <lenkabacinska1@centrum.sk>
2013-11-26 19:00:20 +0100

    Added functions for concrete formats parsing

--- GPSParser/parser.h
+++ GPSParser/parser.h
@@ -27,7 +27,125 @@ private:
      */
     int Count( const std::string & str, const std::string & obj );
 
-    //methods for individual formats
+    /**
+     * @brief minus_colon_dms
+     * processes coordinates from the given string input in format (-)ddd:mm:ss (-)ddd:mm:ss
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate minus_colon_dms(std::string line);
+
+    /**
+     * @brief minus_colon_dm
+     * processes coordinates from the given string input in format (-)ddd:mm.mmmmm (-)ddd:mm.mmmmm
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate minus_colon_dm(std::string line);
+
+    /**
+     * @brief minus_degree_dms
+     * processes coordinates from the given string input in format (-)ddd°mm'ss", (-)ddd°mm'ss"
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate minus_degree_dms(std::string line);
+
+    /**
+     * @brief minus_degree_dm
+     * processes coordinates from the given string input in format (-)ddd°mm.mmmmm', (-)ddd°mm.mmmmm'
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate minus_degree_dm(std::string line);
+
+    /**
+     * @brief minus_d
+     * processes coordinates from the given string input in format (-)ddd.dddddd, (-)ddd.dddddd
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate minus_d(std::string line);
+
+    /**
+     * @brief compas_colon_dms
+     * processes coordinates from the given string input in format [NS]ddd:mm:ss [WE]ddd:mm:ss
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate compas_colon_dms(std::string line);
+
+    /**
+     * @brief compas_colon_dm
+     * processes coordinates from the given string input in format [NS]ddd:mm.mmmmm [WE]ddd:mm.mmmmm
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate compas_colon_dm(std::string line);
+
+    /**
+     * @brief compas_degree_dms
+     * processes coordinates from the given string input in format [NS]ddd°mm'ss" [WE]ddd°mm'ss"
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate compas_degree_dms(std::string line);
+
+    /**
+     * @brief compas_degree_dm
+     * processes coordinates from the given string input in format [NS]ddd°mm.mmmmm' [WE]ddd°mm.mmmmm'
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate compas_degree_dm(std::string line);
+
+    /**
+     * @brief compas_d
+     * processes coordinates from the given string input in format [NS]ddd.dddddd [WE]ddd.dddddd
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate compas_d(std::string line);
+
+    /**
+     * @brief colon_dms_compas
+     * processes coordinates from the given string input in format ddd:mm:ss[NS] ddd:mm:ss[WE]
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate colon_dms_compas(std::string line);
+
+    /**
+     * @brief colon_dm_compas
+     * processes coordinates from the given string input in format ddd:mm.mmmmm[NS] ddd:mm.mmmmm[WE]
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate colon_dm_compas(std::string line);
+
+    /**
+     * @brief degree_dms_compas
+     * processes coordinates from the given string input in format ddd°mm'ss"[NS] ddd°mm'ss"[WE]
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate degree_dms_compas(std::string line);
+
+    /**
+     * @brief degree_dm_compas
+     * processes coordinates from the given string input in format ddd°mm.mmmmm'[NS] ddd°mm.mmmmm'[WE]
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate degree_dm_compas(std::string line);
+
+    /**
+     * @brief d_compas
+     * processes coordinates from the given string input in format ddd.dddddd[NS] ddd.dddddd[WE]
+     * @param line string input with coordinates
+     * @return coordinates parsed to coordinate class
+     */
+    Coordinate d_compas(std::string line);
 
 };
 
